2b920e6410bad00342a559a3f0572761c4de79d6
Andres Ramirez <andres200493@gmail.com>
2021-07-05 21:48:01 -0500

    E1 - E2 - E3 - R1 -> S7

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 $("[data-toggle=popover]").popover();
+
+// Crear proyecto de node
+
+// npm init -> agregar la configuración
+
+// npm i sass -> instalar dependencia de sass desde https://www.npmjs.com/package/sass
+
+// crear .gitignore y agregar texto de la pagina -> https://www.toptal.com/developers/gitignore/api/node
+
+// ejecutar comando -> ./node_modules/sass/sass.js --watch ./scss/main.scss output.css
