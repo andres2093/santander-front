3c1a8855a2126a8da4b8a5560d8a328fff6ea800
Andres Ramirez <andres200493@gmail.com>
2021-07-07 21:26:37 -0500

    Completar funcionalidad de envio de correos

--- app.js
+++ app.js
@@ -43,15 +43,42 @@ const getTemplate = () => {
     .then((response) => response.text())
 }
 
+const sendEmailToApi = (address, template) => {
+  fetch(`https://bedu-email-sender-api.herokuapp.com/send?id`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      address: address,
+      template: template,
+    }),
+  })
+    .then((results) => {
+      console.log(results.status);
+      if(results.status == 200){
+        alert("E-mail send!!!")
+      } else {
+        alert("Send failed")
+      }
+      document.getElementById("email").value = ""
+    })
+    .catch((error) => {
+      console.error(error);
+      document.getElementById("email").value = ""
+      alert("Send failed")
+    });
+};
+
 function sendEmail(miVariable) {
   miVariable.preventDefault()
   const email = miVariable.target.querySelector("input").value
   getTemplate()
-    .then((response) => {
-      console.log(response);
+    .then((template) => {
+      sendEmailToApi(email, template)
     })
     .catch((error) => {
-      console.log(error, "error al obtener el templete");
+      console.log(error, "error al obtener el template");
     })
 }
 
