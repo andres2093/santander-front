3caf01b760d16cc386be7b47eda8d6de2a4f470a
Andres Ramirez <andres200493@gmail.com>
2021-07-07 20:40:26 -0500

    Funcion con JS -> submit

--- app.js
+++ app.js
@@ -11,3 +11,56 @@ $("[data-toggle=popover]").popover();
 // ejecutar comando -> ./node_modules/sass/sass.js --watch ./scss/main.scss output.css
 
 // Si es desde windows -> node ./node_modules/sass/sass.js --watch ./scss/main.scss output.css
+
+
+// Variables -----------------------------------------------------------
+
+// const x = 5
+// let y = 5
+
+// console.log(y);
+
+// y = 6
+
+// var z = 6
+// z = 8
+
+// console.log(z);
+// ------------------------------------------------------------
+
+
+
+const forms = document.querySelectorAll(".signup-form")
+
+// console.log(forms);
+
+// function suma(a, b) {
+//   return a + b
+// }
+
+const getTemplate = () => {
+  return fetch("./template.html")
+    .then((response) => response.text())
+}
+
+function sendEmail(miVariable) {
+  miVariable.preventDefault()
+  const email = miVariable.target.querySelector("input").value
+  getTemplate()
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error, "error al obtener el templete");
+    })
+}
+
+// const sendEmail = (miVariable) => {
+//   miVariable.preventDefault()
+//   console.log(miVariable);
+// }
+
+for(let i = 0; i < forms.length; i++){
+  // console.log(forms[i]);
+  forms[i].addEventListener("submit", sendEmail)
+}
